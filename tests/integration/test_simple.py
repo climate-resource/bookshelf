6e11d1fddc870bf3e531d18d539ebd8589223e31
Jared Lewis <jared@jared.kiwi.nz>
2022-07-16 19:49:03 +1000

    Fix integration tests

--- tests/integration/test_simple.py
+++ tests/integration/test_simple.py
@@ -2,36 +2,24 @@ import pytest
 import scmdata
 import scmdata.testing
 
-from bookshelf import _Book
+from bookshelf import LocalBook, BookShelf
 
 
 def test_simple_book(local_bookshelf, remote_bookshelf):
     remote_bookshelf.register("test", "v1.1.0")
 
-    book = _Book("test", local_bookshelf=local_bookshelf)
-    book.fetch()
+    shelf = BookShelf(path=local_bookshelf)
+    book = shelf.load("test")
 
     expected_dir = local_bookshelf / "test"
     assert (expected_dir / "volume.json").exists()
     assert (expected_dir / "v1.1.0" / "datapackage.json").exists()
 
 
-def test_existing(local_bookshelf, remote_bookshelf):
-    remote_bookshelf.register("test", "v1.1.0")
-
-    book = _Book("test", local_bookshelf=local_bookshelf)
-    book.fetch()
-    remote_bookshelf.mocker.reset()
-
-    new_book = _Book("test", "v1.1.0", local_bookshelf=local_bookshelf)
-    new_book.fetch()
-    assert remote_bookshelf.mocker._adapter.call_count == 1
-
-    assert book.metadata() == book.metadata()
-
-
 def test_adding(local_bookshelf, example_data):
-    book = _Book.create_new("my_new_book", version="v0.1.0")
+    book = LocalBook.create_new(
+        "my_new_book", version="v0.1.0", local_bookshelf=local_bookshelf
+    )
     assert len(book.metadata().resources) == 0
 
     book.add_timeseries("test", example_data)
