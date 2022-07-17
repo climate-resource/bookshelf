882baa359d4713ff47daf581702a523cd491eafb
Jared Lewis <jared@jared.kiwi.nz>
2022-07-17 21:54:26 +1000

    Add tests for is_available

--- tests/unit/test_shelf.py
+++ tests/unit/test_shelf.py
@@ -9,6 +9,12 @@ from bookshelf.errors import UnknownBook, UnknownVersion
 from bookshelf.shelf import BookShelf, LocalBook
 
 
+@pytest.fixture()
+def shelf(local_bookshelf):
+    shelf = BookShelf(path=local_bookshelf)
+    return shelf
+
+
 def test_local_cache():
     shelf = BookShelf()
 
@@ -16,15 +22,13 @@ def test_local_cache():
     assert shelf.path == exp
 
 
-def test_load_missing(local_bookshelf):
-    shelf = BookShelf(path=local_bookshelf, remote_bookshelf="http://example.com")
+def test_load_missing(shelf):
     with pytest.raises(UnknownBook, match=re.escape("No metadata for 'missing'")):
         shelf.load("missing")
 
 
-def test_load_missing_version(local_bookshelf, remote_bookshelf):
+def test_load_missing_version(shelf, remote_bookshelf):
     remote_bookshelf.mocker.get("/v0.1.0/test/v1.1.1/datapackage.json", status_code=404)
-    shelf = BookShelf(path=local_bookshelf)
     with pytest.raises(UnknownVersion, match="Could not find test@v1.1.1"):
         shelf.load("test", "v1.1.1")
 
@@ -32,9 +36,7 @@ def test_load_missing_version(local_bookshelf, remote_bookshelf):
         shelf.load("test", "v1.1.1", force=True)
 
 
-def test_load(remote_bookshelf, local_bookshelf):
-    shelf = BookShelf(path=local_bookshelf)
-
+def test_load(remote_bookshelf, shelf):
     book = shelf.load("test")
     assert book.version == "v1.1.0"
 
@@ -60,9 +62,24 @@ def test_load_with_cached(remote_bookshelf, local_bookshelf):
     assert remote_bookshelf.mocker.call_count == 1
 
 
-def test_save():
-    shelf = BookShelf()
-
+def test_save(shelf):
     book = LocalBook.create_new("test", "v1.0.0")
     with pytest.raises(NotImplementedError):
         shelf.save(book)
+
+
+def test_is_available(shelf, remote_bookshelf):
+    remote_bookshelf.mocker.get("/v0.1.0/other/volume.json", status_code=404)
+
+    assert shelf.is_available("test", "v1.0.0")
+    assert shelf.is_available("test", "v1.1.0")
+
+    assert not shelf.is_available("test", "v1.1.1")
+    assert not shelf.is_available("other", "v1.1.0")
+
+
+def test_is_available_any_version(shelf, remote_bookshelf):
+    remote_bookshelf.mocker.get("/v0.1.0/other/volume.json", status_code=404)
+
+    assert shelf.is_available("test")
+    assert not shelf.is_available("other")
