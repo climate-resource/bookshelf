3e4b9bee2757f9096702cadb50e50de6d450f83e
Jared Lewis <jared@jared.kiwi.nz>
2022-07-20 12:18:28 +1000

    Add core dataset to the wdi book

--- notebooks/wdi.py
+++ notebooks/wdi.py
@@ -14,12 +14,8 @@
 # ---
 
 # %% [markdown]
-# # Example
+# # Worldbank Data Indicators
 #
-# This notebook is an example for the creation of a new book and can be used as a template.
-#
-# In this example, we fetch the RCMIP Radiative Forcing timeseries from `rcmip.org` and
-# produce a resource that contains a filtered subset of variables.
 
 # %%
 import logging
@@ -41,7 +37,7 @@ from bookshelf.notebook import load_nb_metadata
 logging.basicConfig(level=logging.INFO)
 
 # %%
-metadata = load_nb_metadata("example")
+metadata = load_nb_metadata("wdi")
 metadata.dict()
 
 # %% tags=["parameters"]
@@ -60,9 +56,20 @@ local_bookshelf
 # successfully.
 
 # %%
+data_version = "v13"
+expected_hash = "af42db151faf58d3398136b05f8c171db40be921567f536f069f8d1732a3039e"
+
+# %%
+# This can be a little flakey
+# data_fname = pooch.retrieve(
+#     url="http://databank.worldbank.org/data/download/WDI_csv.zip",
+#     known_hash=expected_hash,
+# )
+
+# Data has been cached by CR (hash came from original download)
 data_fname = pooch.retrieve(
-    url="http://databank.worldbank.org/data/download/WDI_csv.zip",
-    known_hash="af42db151faf58d3398136b05f8c171db40be921567f536f069f8d1732a3039e",
+    url=f"http://data.climateresource.com.au/_cache/wdi/{data_version}/WDI_csv.zip",
+    known_hash=expected_hash,
 )
 data_fname
 
@@ -82,10 +89,8 @@ column_rename = {
 df = df.rename(column_rename, axis=1)
 df["scenario"] = "historical"
 df["model"] = "World Bank"
-df["source"] = "WDI_v13"
-df[
-    "unit"
-] = ""  # Units are hard to get. They aren't in the WDISeries.csv column that they should be
+df["source"] = f"WDI_{data_version}"
+df["unit"] = ""
 del df["Unnamed: 66"]
 
 # %%
@@ -113,7 +118,71 @@ def get_units(run):
     return run
 
 
+# This can take a minute or two
 data = data.groupby("variable").map(get_units)
+
+# %% [markdown]
+# ## Emissions cleaning
+
+# %%
+data.filter(variable_code="EN.*").meta[["variable", "unit"]].drop_duplicates()
+
+# %%
+# Check units
+data.meta[data.meta.unit.str.contains("kt")][["variable", "unit"]].drop_duplicates()
+
+# %%
+# Fix emissions units to be emissions/yr
+data["unit"] = data["unit"].replace(
+    "thousand metric tons of CO2 equivalent", "kt CO2-eq/yr"
+)
+data["unit"] = data["unit"].replace("kt of CO2 equivalent", "kt CO2-eq/yr")
+data["unit"] = data["unit"].replace("Mt of CO2 equivalent", "Mt CO2-eq/yr")
+# Check above shows that only emissions ts use "kt" as units
+data["unit"] = data["unit"].replace("^kt$", "kt CO2/yr", regex=True)
+
+# %%
+# Rename variables
+variable_map = {
+    "Agricultural methane emissions": "Emissions|CH4|Agriculture",
+    "Agricultural nitrous oxide emissions": "Emissions|N2O|Agriculture",
+    "CO2 emissions": "Emissions|CO2",
+    "CO2 emissions from electricity and heat production|Total": "Emissions|CO2",
+    "CO2 emissions from gaseous fuel consumption": "Emissions|CO2|Gaseous Fuel Consumption",
+    "CO2 emissions from liquid fuel consumption": "Emissions|CO2|Liquid Fuel Consumption",
+    "CO2 emissions from manufacturing industries and construction": "Emissions|CO2|Manufacturing Industries and Construction",
+    "CO2 emissions from other sectors|Excluding residential buildings and commercial and public services": "Emissions|CO2|Other Sectors",
+    "CO2 emissions from residential buildings and commercial and public services": "Emissions|CO2|Residential Buildings and Commercial and Public Services",
+    "CO2 emissions from solid fuel consumption": "Emissions|CO2|Solid Fuel Consumption",
+    "CO2 emissions from transport": "Emissions|CO2|Transport",
+    "Energy related methane emissions": "Emissions|CH4|Energy",
+    "GHG net emissions/removals by LUCF": "Emissions|GHG|Net LUCF",
+    "HFC gas emissions": "Emissions|HFCs",
+    "Methane emissions": "Emissions|CH4",
+    "Methane emissions in energy sector": "Emissions|CH4|Energy",
+    "Nitrous oxide emissions": "Emissions|N2O",
+    "Nitrous oxide emissions in energy sector": "Emissions|N2O|Energy",
+    "Other greenhouse gas emissions": "Emissions|Other GHGs",
+    "Other greenhouse gas emissions|HFC|PFC and SF6": "Emissions|HFCs|PFC and SF6",
+    "PFC gas emissions": "Emissions|HFCs|PFC",
+    "SF6 gas emissions": "Emissions|HFCs|SF6",
+    "Total greenhouse gas emissions": "Emissions|GHG",
+}
+
+for old, new in variable_map.items():
+    data["variable"] = data["variable"].replace(old, new)
+
+# %%
+data.filter(variable="Emissions*").meta[["variable", "unit"]].drop_duplicates()
+
+# %% [markdown]
+# ## Population
+
+# %%
+data.filter(variable="*Pop*").get_unique_meta("variable")
+
+# %%
+data.filter(variable="Population|*").meta[["variable", "unit"]].drop_duplicates()
 
 # %%
 data.get_unique_meta("variable")
@@ -137,11 +206,24 @@ book = LocalBook.create_new(
     name=metadata.name, version=metadata.version, local_bookshelf=local_bookshelf
 )
 
-# %% [markdown]
-# Create a new `Resource` in the `Book` using the RF `scmdata.ScmRun` object. This function copies the timeseries to a local file and calculate the hash of this file. This hash can be used to check if the files have been modified.
+# %%
+# Entire dataset (~168 MB uncompressed)
+book.add_timeseries("clean", data)
 
 # %%
-book.add_timeseries("clean", data)
+data.filter(variable="GDP").get_unique_meta("unit")
+
+# %%
+# Smaller subset of data that is typically used for analysis
+subset = scmdata.run_append(
+    [
+        data.filter(variable="GDP|PPP", unit="constant 2017 international $"),
+        data.filter(variable="GDP", unit="constant 2015 US$"),
+        data.filter(variable="Emissions|*"),
+        data.filter(variable="Population|Total"),
+    ]
+)
+book.add_timeseries("core", subset)
 
 # %% [markdown]
 # Below the `Book`'s metadata is shown. This contains all of the metadata about the `Book` and the associated `Resources`.
