6cdf78edd116b087dc9a644736428c773f32a0b9
Jared Lewis <jared@jared.kiwi.nz>
2023-11-23 11:11:24 +1100

    chore: Migrate to fixure

--- tests/unit/test_schema.py
+++ tests/unit/test_schema.py
@@ -3,52 +3,42 @@ import pytest
 from bookshelf.schema import DatasetMetadata, NotebookMetadata
 
 
+@pytest.fixture
+def notebook_metadata():
+    return NotebookMetadata(
+        name="test",
+        version="v1.0.0",
+        edition=1,
+        license="unspecified",
+        source_file="",
+        private=False,
+        metadata={},
+        dataset={
+            "author": "test",
+            "files": [{"url": "file://local/filename.txt", "hash": "myhash"}],
+        },
+    )
+
+
 def test_file():
     res = DatasetMetadata(author="test")
     assert res.files == []
 
 
 @pytest.mark.parametrize("idx", (None, 0, -1))
-def test_download_files(idx):
+def test_download_files(idx, notebook_metadata):
     # doesn't trigger pooch.retrieve
     # TODO: add tests for pooch functionality
-    nb_metadata = NotebookMetadata(
-        name="test",
-        version="v1.0.0",
-        edition=1,
-        license="unspecified",
-        source_file="",
-        private=False,
-        metadata={},
-        dataset={
-            "author": "test",
-            "files": [{"url": "file://local/filename.txt", "hash": "myhash"}],
-        },
-    )
 
     if idx:
-        res = nb_metadata.download_file(idx)
+        res = notebook_metadata.download_file(idx)
     else:
-        res = nb_metadata.download_file()
+        res = notebook_metadata.download_file()
 
     assert res == "local/filename.txt"
 
 
 @pytest.mark.parametrize("idx", (-100, 999))
-def test_download_files_with_invalid_idx(idx):
-    nb_metadata = NotebookMetadata(
-        name="test",
-        version="v1.0.0",
-        edition=1,
-        license="unspecified",
-        source_file="",
-        private=False,
-        metadata={},
-        dataset={
-            "author": "test",
-            "files": [{"url": "file://local/filename.txt", "hash": "myhash"}],
-        },
-    )
-
+def test_download_files_with_invalid_idx(idx, notebook_metadata):
     with pytest.raises(ValueError, match="Requested index does not exist"):
-        nb_metadata.download_file(idx)
+        notebook_metadata.download_file(idx)
