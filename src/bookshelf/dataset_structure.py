722005061f4d84d2c3c1ee7e4b3959b8712be409
yinilai <yinilai1021@gmail.com>
2023-12-04 12:59:04 +1100

    revise function output type

--- src/bookshelf/dataset_structure.py
+++ src/bookshelf/dataset_structure.py
@@ -11,7 +11,7 @@ from scmdata import ScmRun
 
 
 # %%
-def get_dataset_dictionary(data: ScmRun) -> dict[str, str]:
+def get_dataset_dictionary(data: ScmRun) -> dict[str, str | float | int]:
     """
     Extract unique metadata values from an ScmRun object.
 
