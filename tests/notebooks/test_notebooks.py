3f18a753687b0564c0056278b52480b400e00e80
yinilai <yinilai1021@gmail.com>
2024-01-17 15:22:32 +1100

    resolve issues

--- tests/notebooks/test_notebooks.py
+++ tests/notebooks/test_notebooks.py
@@ -5,7 +5,8 @@ import tempfile
 from glob import glob
 
 import pytest
-from scmdata import testing
+from attrs import define, field
+from scmdata import ScmRun, testing
 
 from bookshelf import BookShelf
 from bookshelf.dataset_structure import get_dataset_dictionary
@@ -74,39 +75,42 @@ def test_notebook(notebook_path, notebook_name, notebook_version, output_directo
     )
 
 
-def verify_data_dictionary(data, yaml_file):
-    verification = {
-        "col_name_macth": True,
-        "col_type_match": True,
-        "controlled_vocabulary_match": True,
-        "non_na_col_match": True,
-    }
-    if len(yaml_file.data_dictionary) == 0:
+@define
+class verification:
+    col_name_match: bool = field(init=False, default=True)
+    col_type_match: bool = field(init=False, default=True)
+    controlled_vocabulary_match: bool = field(init=False, default=True)
+    non_na_col_match: bool = field(init=False, default=True)
+
+
+def verify_data_dictionary(data: ScmRun, notebook_config: NotebookMetadata) -> verification:
+    verificat = verification()
+    if len(notebook_config.data_dictionary) == 0:
         return None
-    else:
-        unique_values = get_dataset_dictionary(data)
-        for variable in yaml_file.data_dictionary:
-            if variable.name not in data.meta.columns:
-                verification["col_name_macth"] = False
-            else:
-                try:
-                    data.meta[variable.name].astype(variable.type)
-                except ValueError:
-                    verification["col_type_match"] = False
-                if variable.controlled_vocabulary is not None:
-                    if not set(unique_values[variable.name]).issubset(
-                        set(
-                            [
-                                controlled_vocabulary.value
-                                for controlled_vocabulary in variable.controlled_vocabulary
-                            ]
-                        )
-                    ):
-                        verification["controlled_vocabulary_match"] = False
-                if variable.required is True:
-                    if data.meta[variable.name].isna().any():
-                        verification["non_na_col_match"] = False
-        return verification
+
+    unique_values = get_dataset_dictionary(data)
+    for variable in notebook_config.data_dictionary:
+        if variable.name not in data.meta.columns:
+            verificat.col_name_match = False
+        else:
+            try:
+                data.meta[variable.name].astype(variable.type)
+            except ValueError:
+                verificat.col_type_match = False
+            if variable.controlled_vocabulary is not None:
+                if not set(unique_values[variable.name]).issubset(
+                    set(
+                        [
+                            controlled_vocabulary.value
+                            for controlled_vocabulary in variable.controlled_vocabulary
+                        ]
+                    )
+                ):
+                    verificat.controlled_vocabulary_match = False
+            if variable.required is True:
+                if data.meta[variable.name].isna().any():
+                    verificat.non_na_col_match = False
+    return verificat
 
 
 def test_verify_data_dictionary_col_name_macth():
@@ -175,7 +179,7 @@ def test_verify_data_dictionary_col_name_macth():
     expected_columns = {entry.name for entry in config.data_dictionary}
     actual_columns = set(data.meta.columns)
     expected_result = expected_columns.issubset(actual_columns)
-    assert verification["col_name_macth"] == expected_result
+    assert verification.col_name_match == expected_result
 
 
 def test_verify_data_dictionary_col_type_match():
@@ -247,7 +251,7 @@ def test_verify_data_dictionary_col_type_match():
                 type_mismatch = True
                 break
 
-    assert verification["col_type_match"] != type_mismatch
+    assert verification.col_type_match != type_mismatch
 
 
 def test_verify_data_dictionary_controlled_vocabulary_match():
@@ -320,7 +324,7 @@ def test_verify_data_dictionary_controlled_vocabulary_match():
                 cv_mismatch = True
                 break
 
-    assert verification["controlled_vocabulary_match"] != cv_mismatch
+    assert verification.controlled_vocabulary_match != cv_mismatch
 
 
 def test_verify_data_dictionary_non_na_col_match():
@@ -389,7 +393,7 @@ def test_verify_data_dictionary_non_na_col_match():
                 non_na_mismatch = True
                 break
 
-    assert verification["non_na_col_match"] != non_na_mismatch
+    assert verification.non_na_col_match != non_na_mismatch
 
 
 def run_notebook_and_check_results(notebook, version, notebook_dir, output_directory):
@@ -402,19 +406,23 @@ def run_notebook_and_check_results(notebook, version, notebook_dir, output_direc
             output_directory=output_directory,
             version=version,
         )
-        yaml_file = load_nb_metadata(notebook, version, notebook_dir)
+        nb_metadata = load_nb_metadata(notebook, version, notebook_dir)
         files_names = [i["name"] for i in target_book.metadata()["resources"]]
 
         for name in files_names:
             data = target_book.timeseries(name)
-            verification = verify_data_dictionary(data, yaml_file)
-            if verification:
-                for k, v in verification.items():
-                    if not v:
-                        pytest.xfail(f"Data dictionary is incorrect {k} is not true")
+            verification = verify_data_dictionary(data, nb_metadata)
 
+            if verification:
+                for attr in dir(verification):
+                    if not attr.startswith("__"):
+                        result = getattr(verification, attr)
+                        if not result:
+                            pytest.xfail(
+                                f"Data dictionary does not match the data: {attr} is not true"
+                            )
             else:
-                print(f"WARNING: {notebook} does not contain data dictionary")
+                logger.warning(f"{notebook} does not contain data dictionary")
 
     except UnknownBook:
         logger.info("Book has not been pushed yet")
