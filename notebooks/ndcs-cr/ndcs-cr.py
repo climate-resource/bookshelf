fd285cf69e5d0972715dc2f3fcfc1ed7c5b02334
Jared Lewis <jared@jared.kiwi.nz>
2024-02-08 11:33:49 +1100

    chore: Minor changes before publishing

--- notebooks/ndcs-cr/ndcs-cr.py
+++ notebooks/ndcs-cr/ndcs-cr.py
@@ -1,6 +1,5 @@
 # %%
 import logging
-import re
 import tempfile
 
 import scmdata
@@ -25,18 +24,17 @@ book = LocalBook.create_from_metadata(metadata, local_bookshelf=local_bookshelf)
 
 # %%
 for idx, file in enumerate(metadata.dataset.files):
-    pattern = r"(\d{1,2}[A-Za-z]{3}\d{2,4}[a-z]_[\w-]+|\d{8}c_[\w-]+)(?=\.)"
-    file_name = re.findall(pattern, file.url)[0]
     file_data = scmdata.ScmRun(metadata.download_file(idx))
     scenario_name = file_data.get_unique_meta("scenario")
     variant = scenario_name[0].split("_")[-1]
     if variant in ["TP", "CR"]:
-        file_data["scenario"] = "TP"
+        variant_clean = "TP"
     elif variant in ["AVIVA", "CR-noOverrides"]:
-        file_data["scenario"] = "CR"
+        variant_clean = "CR-noOverrides"
     else:
         raise ValueError(f"Unknown variant: {variant}")
-    book.add_timeseries(file_name, file_data)
+    file_data["variant"] = variant_clean
+    book.add_timeseries(variant_clean, file_data)
 
 
 # %%
