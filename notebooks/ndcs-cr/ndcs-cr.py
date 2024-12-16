9ff332412d0831d5cb9229ad652c14adcb2b36f9
Mika Pflüger <mika.pflueger@climate-resource.com>
2024-12-16 16:26:23 +0100

    refactor: update variant naming scheme

--- notebooks/ndcs-cr/ndcs-cr.py
+++ notebooks/ndcs-cr/ndcs-cr.py
@@ -42,16 +42,8 @@ for idx, file in enumerate(metadata.dataset.files):
     file_data = scmdata.ScmRun(metadata.download_file(idx))
     scenario_name = file_data.get_unique_meta("scenario")
     variant = scenario_name[0].split("_")[-1]
-    if variant in ["TP", "CR"]:
-        variant_clean = "TP"
-    elif variant in ["AVIVA", "CR-noOverrides"]:
-        variant_clean = "CR-noOverrides"
-    elif variant in ["UNFCCC"]:
-        variant_clean = "UNFCCC"
-    else:
-        raise ValueError(f"Unknown variant: {variant}")
-    file_data["variant"] = variant_clean
-    book.add_timeseries(variant_clean, file_data)
+    file_data["variant"] = variant
+    book.add_timeseries(variant, file_data)
 
 
 # %%
